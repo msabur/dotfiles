8f8b5cb6888e02e36d28d608ea9e1a9f9b45b22e
msabur <58122827+msabur@users.noreply.github.com>
2025-11-16 02:51:21 -0500

    updates

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    fish_add_path ~/.local/bin
     command -q pyenv && pyenv init - fish | source
     command -q zoxide && zoxide init fish | source
     set -g fish_greeting
